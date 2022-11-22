b33ab412dbb780b8cc454cf672a82ebf0d705709
mikael epelbaum <mikaelepelbaum@gmail.com>
2022-11-23 01:35:11 +0200

    sanity test of first part works

--- ex1.py
+++ ex1.py
@@ -16,13 +16,14 @@ class TaxiProblem(search.Problem):
         """Don't forget to implement the goal test
         You should change the initial to your own representation.
         search.Problem.__init__(self, initial) creates the root node"""
-        # do we really need the self.taxis_start?
-        self.taxis_start =initial["taxis"].copy()
-        taxis = initial["taxis"]
-        # taxi: current state, passagengers, fuel, max capacity
-        taxis = {taxi: (taxis.get(taxi), [], taxis.get(taxi)["fuel"]) for taxi in taxis.keys()}
-        passengers = initial["passengers"]
         self.map = np.array(initial["map"])
+        taxis = initial["taxis"]
+        # fuel level changing bugs the priority queue
+        # taxis = {taxi: (taxis.get(taxi), [], taxis.get(taxi)["fuel"]) for taxi in taxis.keys()}
+        taxis = {taxi: (taxis.get(taxi), []) for taxi in taxis.keys()}
+        passengers = initial["passengers"]
+        # boolean False to indicate passenger wasn't picked yet
+        passengers = {passenger: (passengers.get(passenger), False) for passenger in passengers.keys()}
         state = {'taxis': taxis, 'passengers': passengers, 'remains': len(passengers)}
 
         search.Problem.__init__(self, repr(state))
@@ -38,40 +39,27 @@ class TaxiProblem(search.Problem):
         steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         taxis_dict = state["taxis"]
         taxis_actions = {taxi: [] for taxi in taxis_dict.keys()}
-        passengers_to_collect = set(state["passengers"].keys())
+        passengers = state["passengers"]
+        passengers_to_collect = {passenger for passenger in passengers if not passengers.get(passenger)[1]}
         refuel = []
         for taxi, taxi_details in taxis_dict.items():
             curr_pos = np.asarray(taxi_details[0]["location"])
-            curr_fuel = taxi_details[2]
-            curr_capacity = len(taxi_details[1])
+            # curr_fuel = taxi_details[2]
+            curr_occupancy = len(taxi_details[1])
             all_moves = [curr_pos + np.asarray(step) for step in steps]
             # all possible direction movement listing
             for move in all_moves:
                 if 0 <= move[0] < n and 0 <= move[1] < m:
-                    # bug: list or tuple
                     taxis_actions[taxi].append(("move", taxi, tuple(move)))
-            if taxis_dict[taxi][0]["capacity"] - curr_capacity > 0:
-                # passegers that can be piked listing
-                passengers_to_pick = \
-                    [passenger for passenger, pos in state["passengers"].items() if pos == tuple(curr_pos) and passenger in passengers_to_pick]
-                # redo the following if
-                if len(passengers_to_pick) > 0:
-                    taxis_actions[taxi].append(("pick up", taxi, passengers_to_pick[0]))
-                    passengers_to_collect.remove(passengers_to_pick[0])
+            # passegers that can be piked listing
+            passengers_to_pick = [passenger for passenger in passengers_to_collect if tuple(passengers[passenger][0]["location"]) == tuple(curr_pos)]
+            for pas in passengers_to_pick:
+                taxis_actions[taxi].append(("pick up", taxi, pas))
             # does the taxi has passengers?
             if len(taxis_dict[taxi][1]) > 0:
-                passengers = [passenger for passenger, loc_pos in state["passengers"].items() if loc_pos["destination"] == tuple(curr_pos)]
-                for c in passengers:
-                    for p in taxis_dict[taxi][1]:
-                        if p in state["clients"][c][0]:
-                            taxis_actions[taxi].append(("drop off", taxi, c, p))
-                            state["remains"] = state["remains"] -1
-                            break
-                    else:
-                        continue
-                    break
-
-            # if self.item[taxis_dict[taxi][0]["location"]] == 'G':
+                to_drop_off = [passenger for passenger, loc_pos in passengers.items() if loc_pos[0]["destination"] == loc_pos[0]["location"]]
+                for pas in to_drop_off:
+                    taxis_actions[taxi].append(("drop off", taxi, pas))
             if self.map[tuple([taxis_dict[taxi][0]["location"]][0])] == 'G':
                 refuel = [("refuel", taxi)]
         all_actions = [a for a in taxis_actions.values()]
@@ -96,7 +84,9 @@ class TaxiProblem(search.Problem):
                 if taxi == "wait":
                     continue
                 else:
-                    state["taxis"][taxi][2] = state["taxis"][taxi][0]["fuel"]
+                    # todo: arrange fuel bug
+                    # state["taxis"][taxi][2] = state["taxis"][taxi][0]["fuel"]
+                    state["taxis"][taxi][0]["fuel"] = 15
                     continue
 
             taxi = taxi_a[1]
@@ -105,20 +95,22 @@ class TaxiProblem(search.Problem):
             if taxi_a[0] == "pick up":
                 passenger = taxi_a[2]
                 state["taxis"][taxi][1].append(passenger)
-                del (state["passengers"][passenger])
+                state["passengers"][passenger][1] = True
 
-            if taxi_a[0] == "move":
+
+            if taxi_a[0] == "move" and state["taxis"][taxi][0]["fuel"] > 0:
                 state["taxis"][taxi][0]["location"] = list(taxi_a[2])
-                state["taxis"][taxi][2] = state["taxis"][taxi][2] - 1
+                state["taxis"][taxi][0]["fuel"] = state["taxis"][taxi][0]["fuel"] - 1
+                passengers = state["taxis"][taxi][1]
+                if len(passengers) > 0:
+                    for passenger in passengers:
+                        state["passengers"][passenger][0]["location"] = list(taxi_a[2])
 
             if taxi_a[0] == "drop off":
-                passenger = taxi_a[3]
-                clients = [client for client, det in state["clients"].items() if det[1] == pos and passenger in det[0]]
-                for c in clients:
-                    state["clients"][c][0].remove(passenger)
-                state["to_drop_off"] = state["to_drop_off"] - 1
-                state["taxis"][taxi][1].remove(passenger)
-
+                passenger = taxi_a[2]
+                del state["passengers"][passenger]
+                state['taxis'][taxi_a[1]][1].remove(passenger)
+                state['remains'] -= 1
         return repr(state)
 
     def goal_test(self, state):
